951df5a2342674d538d8d17e5709010ac696811d
brooklyndadcore <scott.waring@gmail.com>
2018-09-28 13:40:26 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,15 @@ require 'ostruct'
 require 'date'
 
 
+
+
 require 'bundler/setup'
 Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 
 # put the code to connect to the database here
